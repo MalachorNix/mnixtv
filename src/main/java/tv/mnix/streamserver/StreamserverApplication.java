4ec5f58a1b187b753181b778339e7f681f2a39fc
malachornix <malachornix@gmail.com>
2021-06-29 23:49:48 +0300

    Java project

---
+++ src/main/java/tv/mnix/streamserver/StreamserverApplication.java
@@ -0,0 +1,13 @@
+package tv.mnix.streamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tv/mnix/streamserver/StreamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.mnix.streamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
